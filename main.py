220ae1dbeac85cd969b5f497794b151bd9110da5
Philliam92 <51342677+Philliam92@users.noreply.github.com>
2022-09-20 13:35:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+#caeser cipher will only work on text; numbers/symbols/spaces remain.
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    else:
+        end_text += char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+from art import logo
+print(logo)
+should_continue = True
+
+while should_continue:
+    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+#allows for user to input any number 
+    shift = shift % 26 
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    restart = input("\nGo again???\nType 'Y' or 'N' ")
+    if restart == 'N' or restart == 'n':
+        should_continue = False
+        print("Goodbye")
+        
